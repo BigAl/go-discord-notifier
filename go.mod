37419556ab292284a18390e17583b116fe8db122
Alan Chalmers <alan@unbnd.com.au>
2021-06-23 22:20:38 +1000

    move to using the embeds module

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module Gopkg.toml
 
 go 1.16
 
-require github.com/aws/aws-lambda-go v1.24.0
+require (
+	github.com/aiomonitors/godiscord v0.0.0-20200422003858-41242d4e5847
+	github.com/aws/aws-lambda-go v1.24.0
+)
--- src/notifier/main.go
+++ src/notifier/main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"bytes"
+
 	"context"
 	"encoding/json"
-	"net/http"
-	"log"
-	"io/ioutil"
+
+
+    "log"
 	"os"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aiomonitors/godiscord"
 )
 
 // Response is of type APIGatewayProxyResponse since we're leveraging the
@@ -19,87 +21,31 @@ import (
 // https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
 type Response events.APIGatewayProxyResponse
 
-// Handler is our lambda handler invoked by the `lambda.Start` function call
-func Handler(ctx context.Context) (Response, error) {
-	var buf bytes.Buffer
-
-	body, err := json.Marshal(map[string]interface{}{
-		"message": "Go Serverless v1.0! Your function executed successfully!",
-	})
-	if err != nil {
-		return Response{StatusCode: 404}, err
-	}
-	json.HTMLEscape(&buf, body)
-
-	resp := Response{
-		StatusCode:      200,
-		IsBase64Encoded: false,
-		Body:            buf.String(),
-		Headers: map[string]string{
-			"Content-Type":           "application/json",
-			"X-MyCompany-Func-Reply": "hello-handler",
-		},
-	}
-    
-//	message := map[string]interface{}{
-//		"hello": "world",
-//		"life":  42,
-//		"embedded": map[string]string{
-//			"yes": "of course!",
-//		},
-//	}
-
-//	bytesRepresentation, err := json.Marshal(message)
-//	if err != nil {
-//		log.Fatalln(err)
-//	}
-
-//	discordresp, err := http.Post("***REMOVED***4", "application/json", bytes.NewBuffer(bytesRepresentation))
-//	if err != nil {
-//		log.Fatalln(err)
-//	}
-
-//	var result map[string]interface{}
-
-//	json.NewDecoder(discordresp.Body).Decode(&result)
-
-//	log.Println(result)
-//	log.Println(result["data"])
-    discordpost()
-
-	return resp, nil
+type DiscordMessage struct {
+	username    string          `json:"username"`
+	avatar_url  string          `json:"avatar_url"`
+	content 	string          `json:"content"`
+	embeds     	json.RawMessage `json:"detail"`
 }
 
-func discordpost() {
+// Handler is our lambda handler invoked by the `lambda.Start` function call
+func Handler(ctx context.Context, event events.CloudWatchEvent) {
+		fmt.Printf("Detail = %s\n", event.Detail)
+
 
 	// Authentication Token pulled from environment variable DISCORD_WEBHOOK_URL
 	WEBHOOKURL := os.Getenv("DISCORD_WEBHOOK_URL")
 	if WEBHOOKURL == "" {
+		log.Fatal("WEBHOOK URL not set")
 		return
 	}
 
-//Encode the data
-postBody, _ := json.Marshal(map[string]string{
-	"content":  "Alan Test hook via variable",
- })
-
- 
- responseBody := bytes.NewBuffer(postBody)
-//Leverage Go's HTTP Post function to make request
- resp, err := http.Post(WEBHOOKURL, "application/json", responseBody)
-//Handle Error
- if err != nil {
-	log.Fatalf("An Error Occured %v", err)
- }
- defer resp.Body.Close()
-//Read the response body
- body, err := ioutil.ReadAll(resp.Body)
- if err != nil {
-	log.Fatalln(err)
- }
- sb := string(body)
- log.Printf(sb)
-
+	embed := godiscord.NewEmbed("AWS Event", string(event.Source), "https://stackoverflow.com/questions/53935198/in-my-discord-webhook-i-am-getting-the-error-embeds-0")
+	embed.AddField("This is a field", "This is the value", true)
+	err := embed.SendToWebhook(WEBHOOKURL)
+    if err != nil {
+        log.Fatal(err)
+    }
 }
 
 func main() {
