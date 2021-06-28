58c1de8c943571f780c2699496127bf5d45da958
Alan Chalmers <alan@unbnd.com.au>
2021-06-28 22:32:30 +1000

    parse the event detail for better formating

--- src/notifier/main.go
+++ src/notifier/main.go
@@ -2,20 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/aiomonitors/godiscord"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
-
-// Response is of type APIGatewayProxyResponse since we're leveraging the
-// AWS Lambda Proxy Request functionality (default behavior)
-//
-// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
-type Response events.APIGatewayProxyResponse
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(ctx context.Context, event events.CloudWatchEvent) {
@@ -28,10 +25,47 @@ func Handler(ctx context.Context, event events.CloudWatchEvent) {
 		return
 	}
 
-	embed := godiscord.NewEmbed("AWS Event Type", string(event.DetailType), "https://stackoverflow.com/questions/53935198/in-my-discord-webhook-i-am-getting-the-error-embeds-0")
+	embed := godiscord.NewEmbed(string(event.DetailType), strings.Join(event.Resources, " "), "https://stackoverflow.com/questions/53935198/in-my-discord-webhook-i-am-getting-the-error-embeds-0")
 	embed.AddField("Account ID", string(event.AccountID), true)
-	embed.AddField("Raw Message Detail", string(event.Detail), true)
-	err := embed.SendToWebhook(WEBHOOKURL)
+
+	//Creating the maps for JSON
+	var m map[string]interface{}
+	err := json.Unmarshal(event.Detail, &m)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	// Start parseing
+	for key, value := range m {
+		switch vv := value.(type) {
+		case string:
+			//fmt.Println(key, ":", vv)
+			embed.AddField(key, vv, false)
+		case float64:
+			//fmt.Println(key, ":", vv)
+			embed.AddField(key, strconv.FormatFloat(vv, 'f', 1, 64), false)
+		case []interface{}:
+			//fmt.Println(key, "is an array:")
+			for i, u := range vv {
+				fmt.Println(i, u)
+			}
+		case map[string]interface{}:
+			//fmt.Println(key, ":")
+			embed.AddField(key, ":", true)
+			for i, u := range vv {
+				//fmt.Println(i, ":", u)
+				embed.AddField(i, u.(string), true)
+			}
+		default:
+			fmt.Println(key, "is of a type I don't know how to handle")
+		}
+	}
+
+	// Add raw message
+	embed.AddField("Raw Message", string(event.Detail), false)
+
+	// Send the embed
+	err = embed.SendToWebhook(WEBHOOKURL)
 	if err != nil {
 		log.Fatal(err)
 	}
